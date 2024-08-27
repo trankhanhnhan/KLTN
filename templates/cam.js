0105dc4a3b3027504b436630791de50f7c24ce08
TranDinhKhanhNhan <trankhanhnhan123123@gmail.com>
2024-08-27 23:10:55 +0700

    Update cam.js

--- templates/cam.js
+++ templates/cam.js
@@ -1,5 +1,5 @@
 // URL của video stream từ camera ESP32
-const streamURL = 'http://<ESP32_IP>:<PORT>/stream';
+const streamURL = 'http://192.168.1.29:5000';
 
 // Gán URL video stream cho phần tử <img>
 document.getElementById('stream').src = streamURL;
