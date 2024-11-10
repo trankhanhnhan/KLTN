e68a12429e99c76003bb0557e2d93c6cceb0c4b4
trankhanhnhan <162677112+trankhanhnhan@users.noreply.github.com>
2024-11-11 01:22:37 +0700

    a

--- YOLO.py
+++ YOLO.py
@@ -63,7 +63,7 @@ def generate_frames():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/')
+@app.route('/video')
 def index():
     return send_file('cam.html')
 
